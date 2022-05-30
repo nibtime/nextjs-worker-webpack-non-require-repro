60072eb7084eb037986464816fb0e7d6ad3cd2bc
nibtime <kf@new.cannabis-med.org>
2022-05-30 12:56:34 +0200

    set up reproduction with next@canary

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import { Html, Main } from 'next/document';
+import Document, {
+  provideComponents,
+} from '@next-safe/middleware/dist/document';
+
+MyDocument.getInitialProps = async (ctx) => {
+  const initialProps = await Document.getInitialProps(ctx);
+  return initialProps;
+};
+
+export default function MyDocument(props) {
+  const { Head, NextScript } = provideComponents(props);
+  return (
+    <Html>
+      <Head>
+        <script
+          data-partytown-config
+          dangerouslySetInnerHTML={{
+            __html: `
+              partytown = {
+                lib: "/_next/static/~partytown/",
+                forward: ["gtag"]           
+              };
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
