6d22ca3855a9f47f140101ff1d3e87e227f998a6
nibtime <kf@new.cannabis-med.org>
2022-06-04 21:16:24 +0200

    add strictDynamic middleware with top-level approach

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import {
+  chain,
+  nextSafe,
+  strictDynamic,
+  reporting,
+} from '@next-safe/middleware';
+
+const isDev = process.env.NODE_ENV === 'development';
+const reportOnly = !!process.env.CSP_REPORT_ONLY;
+
+const nextSafeMiddleware = nextSafe((req) => {
+  return {
+    isDev,
+    contentSecurityPolicy: {
+      reportOnly,
+      'frame-ancestors': 'https://stackblitz.com',
+    },
+    // customize as you need: https://trezy.gitbook.io/next-safe/usage/configuration
+  };
+});
+
+const reportingMiddleware = reporting((req) => {
+  const nextApiReportEndpoint = `/api/reporting`;
+  return {
+    csp: {
+      reportUri: process.env.CSP_REPORT_URI || nextApiReportEndpoint,
+    },
+    reportTo: {
+      max_age: 1800,
+      endpoints: [
+        {
+          url: process.env.REPORT_TO_ENDPOINT_DEFAULT || nextApiReportEndpoint,
+        },
+      ],
+    },
+  };
+});
+
+export default chain(nextSafeMiddleware, strictDynamic(), reportingMiddleware);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,24 @@
 import Document, { Html, Main } from 'next/document';
 import { provideComponents } from '@next-safe/middleware/dist/document';
 
+// when running on Vercel, the response header set by middleware
+// will be found in req, when serving a prod build with next start, it will be in res
+const getCtxHeader = (ctx, header) => {
+  return (
+    ctx.res?.getHeader(header) ||
+    ctx.req?.headers[header] ||
+    ''
+  ).toString();
+};
+
+const CSP_NONCE_HEADER = 'csp-nonce';
+
 MyDocument.getInitialProps = async (ctx) => {
   const initialProps = await Document.getInitialProps(ctx);
+  const nonce = getCtxHeader(ctx, CSP_NONCE_HEADER);
+  if (nonce) {
+    return { ...initialProps, nonce };
+  }
   return initialProps;
 };
 
